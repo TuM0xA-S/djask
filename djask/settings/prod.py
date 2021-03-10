c0131253cb1d51a06064dc60f0c648b0e7f2de62
tum0xa <tum0xa-00@mail.ru>
2021-03-10 14:12:22 +0300

    minor inf fixes

--- djask/settings/prod.py
+++ djask/settings/prod.py
@@ -1,3 +1,5 @@
 from .base import *
 
 DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
--- ep.sh
+++ ep.sh
@@ -1,14 +1,14 @@
-if [ ! -z "$PROD" ]
+if [ ! -z "$DEV" ]
 then
-	echo "production server"
-	export DJANGO_SETTINGS_MODULE=djask.settings.prod
-	./manage.py makemigrations
-	./manage.py migrate
-	gunicorn -w 2 -b :8000 djask.wsgi
-else
 	echo "development server"
 	export DJANGO_SETTINGS_MODULE=djask.settings.dev
 	./manage.py makemigrations
 	./manage.py migrate
 	python3 manage.py runserver 0.0.0.0:8000
+else
+	echo "production server"
+	export DJANGO_SETTINGS_MODULE=djask.settings.prod
+	./manage.py makemigrations
+	./manage.py migrate
+	gunicorn -w 2 -b :8000 djask.wsgi
 fi
